57c6d8fc3af66e1f8a8fbd466ebbb60f0da48fb2
Giuliano Peraz <giuliano.peraz@gmail.com>
2019-03-29 16:40:39 +0100

    Added the part of making figures and feature selection
    I have defined also the criterion for the sequentialfs function to have
    an idea of which features will train the neuronal network among the
    chosen ones.

--- features.m
+++ features.m
@@ -16,13 +16,53 @@ selected_aggr_feat = {@min; @max; @mean; @std};
 selected_feat_num = numel(selected_aggr_feat);
 
 % Arrange data in case they are not multiple of any chunk size
-arranged_data = arrange_data(sensor_data, chunk_size);
+arranged_data = cell([size(sensor_data, 1) chunk_num]);
+for i = 1:size(arranged_data, 1)
+    arranged_data(i, :) = arrange_data(sensor_data{i}, chunk_size);
+end
 
 % Compute features for every element of arranged_data
-feature_cell = cell([numel(selected_feat) numel(chunk_size)]);
-for i = 1:size(feature_cell, 1)
-    for j = 1:numel(chunk_size) 
-        feature_cell{i, j} = get_aggr_features(arranged_data{j}, selected_feat{i}, chunk_size(j));
+feature_cell = cell([categories_num chunk_num selected_feat_num]);
+fig_titles = cell(size(feature_cell));
+for i = 1:categories_num
+    for j = 1:chunk_num 
+        for k = 1:selected_feat_num
+            feature_cell{i, j, k} = get_aggr_features(arranged_data{i, j}, selected_aggr_feat{k}, chunk_size(j));
+        end
     end
 end
 
+% Create and store figures if the user wants it to.
+if STORE_FIGURES == 1
+    for i = 1:categories_num
+        for j = 1:chunk_num 
+            for k = 1:selected_feat_num
+                % Create titles for figures
+                fig_title = sprintf('%s %s', CATEGORIES{i}, func2str(selected_aggr_feat{k}));
+                fig_subtitle = sprintf('chunk size = %d', chunk_size(j));
+                fig_titles{i, j, k} = {fig_title fig_subtitle};
+            end
+        end
+    end
+    produce_figures;
+end
+
+%% Feature selection. 
+% Create labels for categories
+labels = zeros(categories_num, categories_num);
+labels(1,end) = 1;
+for i = 2:categories_num
+    labels(i, :) = circshift(labels(i - 1, :), 1);
+end
+
+% Merge feature matrices into one and add a label to it
+category_features = cell([categories_num chunk_size]);
+for i = 1:categories_num 
+    for j = 1:chunk_num
+        category_features(i, j) = flatten_cell(feature_cell{i, j, :});
+    end
+end
+
+
+criterion = @(xtrain, ytrain, xtest, ytest) sum(ytest ~= classify(xtest, xtrain, ytrain));
+[inmodel, history] = sequentialfs(criterion, X, Y);
--- produce_figures.m
+++ produce_figures.m
@@ -1,10 +1,24 @@
 % Giuliano Peraz <giuliano.peraz@gmail.com>
 % Make figures of the data and store it if the user wants to
 
-% Exit if no data variable has been loaded into the workspace
-if exist('normalized_data', 'var') ~= 1
-    throw(MException('ISP:VariableNotFound', '%s can''t run without normalized_data variable', mfilename));
+% Exit if there is no variable used to make figures
+if (exist('feature_cell', 'var')) ~= 1
+    throw(MException('ISP:VariableNotFound', '%s can''t run without feature_cell variable', mfilename));
 end
 
-% divide the data into chunks
-chunk_size = 5;
+% Create figure directory if it is not present yet
+if exist(FIG_PATH, 'dir') == false
+    fprintf('Creating %s\n', FIG_PATH);
+    mkdir(FIG_PATH);
+end
+
+for i = 1:numel(feature_cell)
+    figure(i);
+    if ~exist('fig_titles', 'var')
+        fig_title = sprintf('Figure %d', i);
+    else
+        fig_title = fig_titles{i};
+    end
+    bar(feature_cell{i});
+    title(fig_title);
+end
