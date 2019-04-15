bbb2ca27ce016c33e5cc07d152805781fce0cafa
Giuliano Peraz <giuliano.peraz@gmail.com>
2019-04-15 11:11:43 +0200

    Rewrite of the frequency transform function
    It now returns also the frequency domain along side with the frequency
    tranform data

--- freq_transform.m
+++ freq_transform.m
@@ -1,20 +1,13 @@
-function [freq_data] = freq_transform(data)
-if isempty(data)
-    freq_data = [];
-    return;
-end
+function [freq_data, freq_domain] = freq_transform(data, sampling_time)
+% Compute signal length and single side_spectrum of the fft
+sig_len = size(data, 1);
+n = 2^nextpow2(sig_len);
+double_side_spectrum = abs(fft(data, n)/n);
+single_side_spectrum = double_side_spectrum(1:n/2 + 1, :);
 
-% data for this function is project specific
-data_val = data(:, 1:3);
-time_val = data(:, 4);
-sample_time = mean(diff(time_val));
-sig_len = size(data_val, 1);
-double_side_spectrum = abs(fft(data_val)/sig_len);
-single_side_spectrum = double_side_spectrum(1:sig_len/2 + 1, :);
-single_side_spectrum(2:end - 1, :) = 2 * single_side_spectrum(2:end - 1, :);
-
-% add the vector of frequencies
-fs = 1/sample_time;
-f = fs/2 * linspace(0, 1, sig_len/2 + 1);
-freq_data = [single_side_spectrum, f'];
+% Compute frequency domain and return results to caller
+fs = 1/sampling_time;
+f = fs/2 * linspace(0, 1, n/2 + 1);
+freq_data = single_side_spectrum;
+freq_domain = f';
 end
